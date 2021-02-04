21438da843bc4dac2266164884777f0cc42be2fe
Shaun Nicolai <shaunico@gmail.com>
2021-02-04 15:30:28 -0300

    PROJECT CREATED

---
+++ src/main/java/com/example/demo/ProjetoWebSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/ProjetoWebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
